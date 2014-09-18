15c1ebaac75555e6ccd9a17bd542389c3372c115
Josh Meek <joshmeek18@gmail.com>
2014-09-17 22:01:40 -0400

    init

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,20 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(params[:todo].permit(:title, :done))
+
+    if @todo.save
+      redirect_to "/todos"
+    else
+      render :new
+    end
+  end
+
+end
